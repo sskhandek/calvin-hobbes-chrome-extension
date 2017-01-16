f8fe0fb16956a24a8d56d6f4fbaf7812b43b5b61
Sujay Khandekar <khandek2@illinois.edu>
2017-01-15 19:07:06 -0800

    removed time, changed font

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 // Fetch Comic
-
 const url = "http://www.gocomics.com/calvinandhobbes";
+
 $.ajax({
     url: url,
     method: 'GET',
@@ -12,6 +12,7 @@ $.ajax({
       if(el.className === 'amu-container-global') {
           const comicSrc = el.getElementsByClassName("item-comic-image")[0].children[0].src
           $('#comic').attr('src', comicSrc);
+          localStorage.setItem("imgData", imgData);
       }
   });
 });
@@ -19,15 +20,6 @@ $.ajax({
 // Get the time
 function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-    const fullTime = `${hours}:${minutes}:${("0" + seconds).slice(-2)}`;
-    $('#hours').html(("0" + hours).slice(-2));
-    $('#minutes').html(("0" + minutes).slice(-2));
-    $('#seconds').html(("0" + seconds).slice(-2));
-    $('#date').html(now.toDateString());
+    $('#date').html(now.toLocaleDateString());
 }
-
 setDate();
-setInterval(setDate, 1000);
