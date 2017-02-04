4b5769fe4b2e0237dd16856cbe614f9a35d691e0
Sujay Khandekar <khandek2@illinois.edu>
2017-02-03 21:40:26 -0600

    Local Storage Optimization

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 
 // Fetch Comic
 const url = "http://www.gocomics.com/calvinandhobbes";
-var obj = localStorage.getItem('calvinandhobbes_url');
-var currDate = new Date();
+const obj = localStorage.getItem('calvinandhobbes_url');
+const currDate = new Date();
 if (obj) {
-    obj = JSON.parse(obj);
-    if (obj.date == currDate.getDate()) {
-        $('#comic').attr('src', obj.src);
-    }
-} else {
-    $.ajax({
-        url: url,
-        method: 'GET',
-        crossDomain: true
-    }).then((responseData) => {
-      html = $.parseHTML(responseData);
-      $.each(html, (i, el) => {
-          if(el.className === 'amu-container-global') {
-              const comicSrc = el.getElementsByClassName("item-comic-image")[0].children[0].src
-              localStorage.setItem('calvinandhobbes_url', JSON.stringify({src: comicSrc, date: currDate.getDate()}));
+    $('#comic').attr('src', obj);
+}
+
+$.ajax({
+    url: url,
+    method: 'GET',
+    crossDomain: true
+}).then((responseData) => {
+  html = $.parseHTML(responseData);
+  $.each(html, (i, el) => {
+      if (el.className === 'amu-container-global') {
+          const comicSrc = el.getElementsByClassName("item-comic-image")[0].children[0].src
+          if (!obj || comicSrc !== obj) {
+              localStorage.setItem('calvinandhobbes_url', comicSrc);
               $('#comic').attr('src', comicSrc);
           }
-      });
-    });
-
-}
+      }
+  });
+});
 
 // Get the time
 function setDate() {
