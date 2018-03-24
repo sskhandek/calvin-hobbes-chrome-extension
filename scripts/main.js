6e2b9bfc2a9e2a10d5e3c4c28c6f47b940dfc3cf
Sujay Khandekar <sskhandek@gmail.com>
2018-03-23 22:26:38 -0700

    Fix for the container

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,46 @@
-
 // Fetch Comic
-const url = "http://www.gocomics.com/calvinandhobbes";
+const url = 'http://www.gocomics.com/calvinandhobbes';
 const obj = localStorage.getItem('calvinandhobbes_url');
 const currDate = new Date();
 if (obj) {
-    $('#comic').attr('src', obj);
+  $('#comic').attr('src', obj);
 }
 
 $.ajax({
-    url: url,
-    method: 'GET',
-    crossDomain: true
-}).then((responseData) => {
-  html = $.parseHTML(responseData);
-  $.each(html, (i, el) => {
-      if (el.className === 'amu-container-global') {
-          const comicSrc = el.getElementsByClassName("item-comic-image")[0].children[0].src
-          if (!obj || comicSrc !== obj) {
-              localStorage.setItem('calvinandhobbes_url', comicSrc);
-              $('#comic').attr('src', comicSrc);
-          }
+  url: url,
+  method: 'GET',
+  crossDomain: true,
+})
+  .then(responseData => {
+    html = $.parseHTML(responseData);
+    $.each(html, (i, el) => {
+      if (el.className && el.className.includes('amu-container-global')) {
+        const comicSrc = el.getElementsByClassName('item-comic-image')[0]
+          .children[0].src;
+        if (!obj || comicSrc !== obj) {
+          localStorage.setItem('calvinandhobbes_url', comicSrc);
+          $('#comic').attr('src', comicSrc);
+        }
       }
+    });
+  })
+  .fail(err => {
+    console(err);
   });
-}).fail((err) => {
-  console(err);
-});
 
 // Get the time
 function setDate() {
-    const now = new Date();
-    $('#date').html(now.toLocaleDateString());
+  const now = new Date();
+  $('#date').html(now.toLocaleDateString());
 }
 setDate();
 
 // // Randomly set background image
 
-let arr = ['background-1.png', 'background-2.png', 'background-3.png']
-let imageUrl = arr[(Math.floor(Math.random() * 3))];
+let arr = ['background-1.png', 'background-2.png', 'background-3.png'];
+let imageUrl = arr[Math.floor(Math.random() * 3)];
 let i = 'url(../images/' + imageUrl + ')  no-repeat center center fixed';
 $('#main').css({
-  'background': i,
-  'background-size': 'cover'
+  background: i,
+  'background-size': 'cover',
 });
-
-
-
-
